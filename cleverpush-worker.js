2303338a4d7c522ec75ef12b3e3d6fe2c5488854
Nishi86 <100294793+Nishi86@users.noreply.github.com>
2026-05-29 16:36:26 +0530

    Update cleverpush-worker.js

--- cleverpush-worker.js
+++ cleverpush-worker.js
@@ -1,6 +1,6 @@
 // importScripts('https://static.cleverpush.com/channel/worker/ByfNxRPcs8ZcGAsyv.stage.js' + self.location.search);
 self.cleverpushConfig = {
-  channelId: 'ByfNxRPcs8ZcGAsyv',
-  apiEndpoint: 'https://api-stage.cleverpush.com',
+  channelId: 'zsKod4ggm8r8XzCev',
+  apiEndpoint: 'https://api.cleverpush.com',
 };
-importScripts("https://static.cleverpush.com/sdk/cleverpush-worker.stage.js");
+importScripts("https://static.cleverpush.com/sdk/cleverpush-worker.js");
